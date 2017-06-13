07833936858f485d1a92633930b40ed49162e3d2
ali <ali@ali-PC.admin.ad.cnm.edu>
2017-06-13 16:15:36 -0600

    changes to fizz buzz for upper limits and usage

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,12 +3,20 @@
 public class FizzBuzz {
   /** Default upper limit for counting */
   public static final int DEFAULT_UPPER_LIMIT = 100;
+  public static final String USAGE = "Usage:%n"
+      + "\tjava [options] edu.cnm.deepdive.fizzbuzz.FizzBuzz [limit]%n" + "%n" + "Where%n"
+      + "\toptions = VM options%n" + "\tlimit = upper counting limit (default = 100 %n";
 
 
   public static void main(String[] args) {
-    int UpperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
-    emitFizzBuzz(UpperLimit);
-
+    try {
+      int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
+      if (upperLimit <= 0) {
+        emitFizzBuzz(upperLimit);
+      }
+    } catch (NumberFormatException ex) {
+      emitUsage();
+    }
   }
 
   /**
@@ -33,4 +41,7 @@ public class FizzBuzz {
     }
   }
 
+  private static void emitUsage() {
+    System.out.printf(USAGE);
+  }
 }
